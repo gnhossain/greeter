5e35fd2e02ec0f651b060dd9fa9895dd16f7f113
G H <gh@Gs-MacBook-Pro.local>
2019-10-31 14:51:25 -0700

    fixed

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ server.get('/', (req, res) => {
   res.status(200).json({ hello: 'Web 23, succesful deployment!!!!' });
 });
 
-server.listen(4000, () => {
+ const port = process.env.PORT || 4000;
+server.listen(port, () => {
   console.log('\n Server running on port 4000 \n');
 });
